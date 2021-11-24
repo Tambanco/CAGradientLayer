d3bc21143b799b6b897782c0caeb587f68e0ee4c
Tambanco <52546246+Tambanco@users.noreply.github.com>
2021-11-24 14:21:12 +0300

    add EndPointGradient class

---
+++ CAGradientLayer/EndPointGradientViewController.swift
@@ -0,0 +1,16 @@
+//
+//  EndPointGradientViewController.swift
+//  CAGradientLayer
+//
+//  Created by tambanco 🥳 on 24.11.2021.
+//
+
+import UIKit
+
+class EndPointGradientViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
